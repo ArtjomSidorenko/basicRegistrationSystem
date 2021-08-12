dbbbe11d7d0a88801a605246f6c3e19d18333a21
ArtjomSidorenko <sidorenko.artjom@gmail.com>
2021-08-13 00:16:39 +0300

    cursor variation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,47 +5,108 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>-->
+    <div class="cursor"></div>
 
+    <div class="intro" >
+      <div class="container" >
+        <div class="intro__inner" >
+          <h2 class="intro__suptitle" style="color: white">Login/Register</h2>
+          <h1 class="intro__title">Welcome to the hub</h1>
+          <input class="btn" type="submit" name="" value="Get Started">
 
-    <div class="intro">
-      <div class="container">
-        <div class="intro__inner">
-          <h2 class="intro__suptitle">Creative Template</h2>
-          <h1 class="intro__title">Welcome to MoGo</h1>
-
-          <a class="btn" href="#">Get started</a>
         </div>
       </div>
     </div>
     <div class="cursor"></div>
-    <div class="cursor2"></div>
+
 
 
     </div>
 </template>
 <script>
-let mouseCursor = document.querySelector(".cursor");
+/*let mouseCursor = document.querySelector(".cursor");
 window.addEventListener('mousemove', cursor);
 
 function cursor(e){
   console.log(e)
-  mouseCursor.style.top = e.pageY + 'px';
-  mouseCursor.style.left = e.pageX + 'px';
-}
+  mouseCursor.style.top = e.pageY + "px";
+  mouseCursor.style.left = e.pageX + "px";
+}*/
+/*const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = `${x}px`;
+  cursor.style.left = `${y}px`;
+});*/
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  console.log(e)
+  cursor.setAttribute("style", "top: "+e.pageY+"px; left: "+e.pageX+"px;")
+
+
+});
 </script>
 <style>
+/* Button */
+.btn {
+
+  display: inline-block;
+  vertical-align: top;
+  padding: 8px 30px;
+  margin: auto;
+  text-align: center;
+
+  background: none;
+
+  border: 0;
+  border: 3px solid #fff;
+  border-radius: 24px;
+
+  font-size: 24px;
+  font-weight: 750;
+  color: #fff;
+  text-transform: uppercase;
+  text-decoration: none;
+  outline: none;
+
+  height: 70px;
+  width: 300px;
+  transition: width 1s, background-color 1s;
+}
+
+.btn:hover {
+  background-color: #fff;
+  color: deepskyblue;
+  width: 480px;
+
+
+}
+
 /* cursor */
-cursor{
+.cursor{
   position: absolute;
-  left: 0;
-  top: 0;
-  border: 2px solid white;
+  border: 1px solid white;
   border-radius: 50%;
-  width: 50px;
-  height: 50px;
+  width: 20px;
+  height: 20px;
   pointer-events: none;
-  transform: translate(-50%, -50%);
-  transition: .1s;
+  transition-duration: 200ms;
+  transition-timing-function: ease-out;
+}
+cursor::after{
+  content: "";
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  border: 8px solid gray;
+  border-radius: 50%;
+  opacity: .5;
+  top: -8px;
+  left: -8px;
 }
 
 
@@ -63,7 +124,8 @@ cursor{
   height: 800px;
   line-height: 30px;
   font-size: 20px;
-  text-align: center;
+
+
 }
 
 .container:hover ~ .cursor{
@@ -72,7 +134,25 @@ cursor{
   opacity: .5;
 }
 
-intro{
+.intro{
+
+}
+
+.intro__inner {
+padding: 200px;
+
+}
+
+.intro__suptitle{
+  letter-spacing: 5px;
+}
+
+.intro__title {
+  color: #fff;
+  font-size: 80px;
+  font-weight: 650;
+  text-transform: uppercase;
+  line-height: 1;
 
 }
 
@@ -89,9 +169,11 @@ body {
   padding: 0;
   font-family: "Open Sans", sans-serif;
   color: white;
-  
-}
 
+}
+* {
+  font-family: "Do Hyeon", sans-serif;
+}
 
 /* Scroll */
 
@@ -116,6 +198,8 @@ body {
   background: #4200ff;
   border-radius: 10px;
 }
+
+
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
